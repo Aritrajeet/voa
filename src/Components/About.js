5366f9de6a79e49633dcff2275321eb5dd09ab6b
Aritra Chatterjee <aritrajeet123@gmail.com>
2022-09-07 20:08:35 +0530

    resolved image load err

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./CSS/About.css";
 
 import GUITARPNG from "../Images/electric-guitar.png";
+import PLAY from "../Images/play.png";
+import MUSIC from "../Images/music.png";
 
 export default function About() {
   return (
@@ -11,7 +13,7 @@ export default function About() {
       </h3>
       <div class="container">
         <div class="cards">
-          <img src="./images/play.png" class="im1" />
+          <img src={PLAY} class="im1" />
           <h3 class="addfont">Cover Songs</h3>
           <p class="pcards">
             Majority of my work is on cover songs. Love to sing great melodies
@@ -27,7 +29,7 @@ export default function About() {
           </p>
         </div>
         <div class="cards">
-          <img src="./images/music.png" class="im3" />
+          <img src={MUSIC} class="im3" />
           <h3 class="addfont">Music Production</h3>
           <p class="pcards">
             Learnt a bit of music production. Can create own music. Can also mix
